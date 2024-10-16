8e605a7e3a6f50f294db77dc26cd80799946871c
Touhid Ansari <touhidansari187@gmail.com>
2024-10-16 12:50:03 +0530

    implimenting onboarding page to get user name

--- app/components/SubmitButtons.tsx
+++ app/components/SubmitButtons.tsx
@@ -5,6 +5,30 @@ import Image from "next/image";
 import { Loader2 } from "lucide-react";
 import GoogleLogo from '@/public/google.png'
 import GitHubLogo from '@/public/github.png'
+import { cn } from "@/lib/utils";
+
+interface iAppProps{
+    text: string;
+    variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | null | undefined;
+
+    className?:string,
+}
+
+export function SubmitButton({text,variant,className} : iAppProps){
+    const {pending} = useFormStatus()
+
+    return (
+        <>
+        {pending?(
+            <Button disabled variant='outline' className={cn("w-fit",className)}>
+                <Loader2 className="size-4 mr-2 animate-spin"/> Please wait
+            </Button>
+        ):(
+            <Button type="submit" variant={variant} className={cn("w-fit",className)}>{text}</Button>
+        )}
+        </>
+    )
+}
 
 export function GoogleAuthButton(){
     const {pending} = useFormStatus();
---
+++ app/lib/zodSchemas.ts
@@ -0,0 +1,40 @@
+import { conformZodMessage } from '@conform-to/zod';
+import {z} from 'zod'
+
+export const onboardingSchema=z.object({
+    fullName: z.string().min(3).max(150),
+    userName: z.string().min(3).max(150).regex(/^[a-zA-Z0-9-]+$/,{
+        message:'Username can only conatain letters, number and -',
+    }),
+});
+
+export function onboardingSchemaValidation(options?:{
+    isUsernameUnique:()=> Promise<boolean>;
+}){
+    return z.object({
+        userName: z.string().min(3).max(150).regex(/^[a-zA-Z0-9-]+$/,{
+            message:'Username can only conatain letters, number and -',
+        })
+        .pipe(
+            z.string().superRefine((_,ctx) =>{
+                if(typeof options?.isUsernameUnique !== "function"){
+                    ctx.addIssue({
+                        code:'custom',
+                        message:conformZodMessage.VALIDATION_UNDEFINED,
+                        fatal:true,
+                    });
+                    return;
+                }
+                return options.isUsernameUnique().then((isUnique)=>{
+                    if(!isUnique){
+                        ctx.addIssue({
+                            code:'custom',
+                            message:'Username is already taken',
+                        })
+                    }
+                })
+            })
+        ),
+    fullName: z.string().min(3).max(150),
+    })
+}
